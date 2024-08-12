feb030c00f5a62b55351debca4ce45bcb2a2bbdf
ovidiu.pascal <pascal.ovidiuu@gmail.com>
2024-08-12 16:06:28 +0200

    PROGRAM: Computing the dimensional weight of a box
    Signed-off-by: ovidiu.pascal <pascal.ovidiuu@gmail.com>

--- main.c
+++ main.c
@@ -1,8 +1,41 @@
+/* Compute the dimensional weight of a box from inputs provided by the user
+ *
+ * In the USA, the usual method is to divide the volume by 166
+ *
+ * volume = height * length * width
+ *
+ * REQUIREMENTS --> The company wants us to round up the dimensional weight as they want an integer
+ *
+ * MATH TRICK --> Add (divisor - 1) to the volume before dividing. This ensures any remainder results in rounding up to the next whole number.
+ *
+ ***********************************************************************************************************************
+ * TO BE UPGRADED -> as of now, it doesnt work correctly if the users enters non-numeric input
+*/
+
 #include <stdio.h>
+
+#define INCHES_PER_POUND 166                // Add a constant variable 
 
 int main(void)
 {
-    printf("To C, or not to C: that is the question ?\n");
+    int height, length, width, volume, weigth;
+
+    printf("Enter height of box: ");
+    scanf("%d", &height);                   // write the value input by the user into the integer variable 'heigth'
+
+    printf("Enter length of box: ");
+    scanf("%d", &length);                   // write the value input by the user into the integer variable 'length'
+
+    printf("Enter width of box: ");
+    scanf("%d", &width);                   // write the value input by the user into the integer variable 'width'
+
+    volume = height * length * width;
+
+    weigth = (volume + INCHES_PER_POUND -1) / INCHES_PER_POUND;
+
+    printf("\nVolume (cubic inches): %d\n", volume);            // The OG f-string -> we add an integer variable and we name which one
+    printf("Dimensional weight (pounds) : %d\n", weigth);
+
     return 0;
 }
 
