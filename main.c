606435531518c1c65ecbe411396d590c7f60c8ed
ovidiu.pascal <pascal.ovidiuu@gmail.com>
2024-08-12 17:59:39 +0300

    PROGRAM: Converting from Fahrenheit to Celsius
    Signed-off-by: ovidiu.pascal <pascal.ovidiuu@gmail.com>

--- main.c
+++ main.c
@@ -1,41 +1,20 @@
-/* Compute the dimensional weight of a box from inputs provided by the user
- *
- * In the USA, the usual method is to divide the volume by 166
- *
- * volume = height * length * width
- *
- * REQUIREMENTS --> The company wants us to round up the dimensional weight as they want an integer
- *
- * MATH TRICK --> Add (divisor - 1) to the volume before dividing. This ensures any remainder results in rounding up to the next whole number.
- *
- ***********************************************************************************************************************
- * TO BE UPGRADED -> as of now, it doesnt work correctly if the users enters non-numeric input
-*/
+/* Converting from Fahrenheit temperature to Celsius */
 
 #include <stdio.h>
 
-#define INCHES_PER_POUND 166                // Add a constant variable 
+#define FREEZING_PT 32.0f
+#define SCALE_FACTOR (5.0f / 9.0f)
 
 int main(void)
 {
-    int height, length, width, volume, weigth;
+    float fahrenheit, celsius;
 
-    printf("Enter height of box: ");
-    scanf("%d", &height);                   // write the value input by the user into the integer variable 'heigth'
+    printf("Enter Fahrenheit temperature: ");
+    scanf("%f", &fahrenheit);
 
-    printf("Enter length of box: ");
-    scanf("%d", &length);                   // write the value input by the user into the integer variable 'length'
+    celsius = (fahrenheit - FREEZING_PT) * SCALE_FACTOR;
 
-    printf("Enter width of box: ");
-    scanf("%d", &width);                   // write the value input by the user into the integer variable 'width'
-
-    volume = height * length * width;
-
-    weigth = (volume + INCHES_PER_POUND -1) / INCHES_PER_POUND;
-
-    printf("\nVolume (cubic inches): %d\n", volume);            // The OG f-string -> we add an integer variable and we name which one
-    printf("Dimensional weight (pounds) : %d\n", weigth);
+    printf("Celsius equivalent is: %.1f\n", celsius);
 
     return 0;
 }
-
