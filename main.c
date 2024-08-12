bc5b9425c8783f092ad04ccb578d6adbb6ebcb98
ovidiu.pascal <pascal.ovidiuu@gmail.com>
2024-08-12 18:52:33 +0300

    PROGRAM-C2.2: Compute the volume of a sphere with a 10 meters radius
    Signed-off-by: ovidiu.pascal <pascal.ovidiuu@gmail.com>

--- main.c
+++ main.c
@@ -1,15 +1,24 @@
-/* Write a program to display a certain picture */
+/* Write a program that computes the volume of a sphere with a 10-meters radius
+ *
+ * Use the formula v = (4/3)*pi*r^(3)
+ *
+ * -> Write 4/3 as (4.0f/3.0f)
+ * -> C doesn't have an exponention operator , u'll need to multiply r by itself 3 times
+ * -> radius = r
+ */
 
 #include <stdio.h>
 
+#define PI 3.14159
+#define RADIUS 10
+
 int main(void)
 {
-    printf("         *\n"); // 2 tab
-    printf("        *\n");
-    printf("       *\n");
-    printf(" *    *\n");
-    printf("  *  *\n");
-    printf("   *\n");
+    float volume;
+
+    volume = (4.0f/3.0f) * PI * (RADIUS * RADIUS * RADIUS);
+
+    printf("The volume of a 10-meter radius sphere is: %f", volume);
 
     return 0;
 }
