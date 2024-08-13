b67ef31f93db5aab1f432d3df185e19ac3924a31
ovidiu.pascal <pascal.ovidiuu@gmail.com>
2024-08-13 17:09:02 +0300

    PROGRAM-C3.3: break down an ISBN 13 entered by a user
    Signed-off-by: ovidiu.pascal <pascal.ovidiuu@gmail.com>

--- main.c
+++ main.c
@@ -1,40 +1,34 @@
-/* Write a program that format product information entered by the user.
+/* Write a program that breaks down an ISBN entered by a user
  *
- * HINT: the item number and date should left justified, the unit price should be right justified
- * HINT: Allow for dollar amount up to $9999.99
- * HINT: Use tab
+ * ISBN assigned after January 1, 2007 contains 13 digits, arranged in 5 groups:
+ * Older ISBN contains 10 digits
  *
+ * Groups:
+ * 1. GSI prefix: either 978 or 979
+ * 2. group identifiers specifies the language or the country of origin
+ * 3. The publisher code identifies the publisher
+ * 4. An item number is assigned by the publisher to identify a specific book
+ * 5. An ISBN end with a check digit that is used to verify the accuracy of the preceding digits
+ *
+ * Note: The number of digits in each groups may vary, you can't assume that groups have the length show in this example : 978-0-393-97950-3
+ *
+ * Test yur porogram with actual ISBN
  */
 
 #include <stdio.h>
 
 int main(void)
 {
-    int item_number, mm, dd, yyyy;
-    float price;
+    int gsi_prefix, group_identifier, publisher_code, item_number, check_digit;
 
-    printf("Enter item number: ");
-    scanf("%d", &item_number);
+    printf("Enter ISBN: ");
+    scanf("%d-%d-%d-%d-%d", &gsi_prefix, &group_identifier, &publisher_code, &item_number, &check_digit);
 
-    printf("Enter unit price: ");
-    scanf("%f", &price);
-
-    printf("Enter purchase date (mm/dd/yyyy): ");
-    scanf("%d/%d/%d", &mm, &dd, &yyyy);
-
-    printf("\nItem\t\tUnit\t\tPurchase\n");
-    printf("\t\tPrice\t\tDate\n");
-
-    /* The %7.2f format specifier ensures the price is right-justified within a field of 7 characters,  with 2 digits after the decimal point.
-     * The total width of 7 allows for the maximum value $9999.99.
-     *
-     * Explanation:
-     * - 1 character for the dollar sign ($)
-     * - 4 characters for the digits before the decimal point (up to 9999)
-     * - 1 character for the decimal point (.)
-     * - 2 characters for the digits after the decimal point (.99)
-    */
-    printf("%-d\t\t$%7.2f\t%.2d/%.2d/%d\n", item_number, price, mm, dd, yyyy);
+    printf("\nGSI prefix: %d\n", gsi_prefix);
+    printf("Group identifier: %d\n", group_identifier);
+    printf("Publisher code: %d\n", publisher_code);
+    printf("Item number: %d\n", item_number);
+    printf("Check digit: %d\n", check_digit);
 
 
     return 0;
