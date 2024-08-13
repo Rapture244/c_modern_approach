d06974e0d67429f63cd9778342c90e36b2857878
ovidiu.pascal <pascal.ovidiuu@gmail.com>
2024-08-13 17:14:02 +0300

    PROGRAM-C3.4: Input phone number and output it
    Signed-off-by: ovidiu.pascal <pascal.ovidiuu@gmail.com>

--- main.c
+++ main.c
@@ -1,35 +1,18 @@
-/* Write a program that breaks down an ISBN entered by a user
+/* Write a program that prompts user to enter a phone number in the form (xxx) xxx-xxxx and display it as xxx.xxx.xxxx
  *
- * ISBN assigned after January 1, 2007 contains 13 digits, arranged in 5 groups:
- * Older ISBN contains 10 digits
- *
- * Groups:
- * 1. GSI prefix: either 978 or 979
- * 2. group identifiers specifies the language or the country of origin
- * 3. The publisher code identifies the publisher
- * 4. An item number is assigned by the publisher to identify a specific book
- * 5. An ISBN end with a check digit that is used to verify the accuracy of the preceding digits
- *
- * Note: The number of digits in each groups may vary, you can't assume that groups have the length show in this example : 978-0-393-97950-3
- *
- * Test yur porogram with actual ISBN
+ * Ex: (404) 817-6900
  */
 
 #include <stdio.h>
 
 int main(void)
 {
-    int gsi_prefix, group_identifier, publisher_code, item_number, check_digit;
+    int parenthese, digit_3, digit_4;
 
-    printf("Enter ISBN: ");
-    scanf("%d-%d-%d-%d-%d", &gsi_prefix, &group_identifier, &publisher_code, &item_number, &check_digit);
+    printf("Enter phone number [(xxx) xxx-xxxx]: ");
+    scanf("(%d) %d-%d", &parenthese, &digit_3, &digit_4);
 
-    printf("\nGSI prefix: %d\n", gsi_prefix);
-    printf("Group identifier: %d\n", group_identifier);
-    printf("Publisher code: %d\n", publisher_code);
-    printf("Item number: %d\n", item_number);
-    printf("Check digit: %d\n", check_digit);
-
+    printf("\nYou entered: %d.%d.%d\n", parenthese, digit_3, digit_4);
 
     return 0;
 }
