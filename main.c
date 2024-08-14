8803d56cbc402a53c7353e7eab006ad691da4351
ovidiu.pascal <pascal.ovidiuu@gmail.com>
2024-08-14 22:37:49 +0300

    PROGRAM-C5.5: Input income, output tax amount
    Signed-off-by: ovidiu.pascal <pascal.ovidiuu@gmail.com>

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
-/* Write a program that ask the user for a 24-hour time, then displays the 12-hour time equivalent
+/* Write a program that ask the user his income and give back the tax amount
+*
+ * Income               Amounf of tax
+ * Not over $750        1% of income
+ * 750-2 2250           $7.50 + 2% of the amount
+ * 2 250-3 750          $37.50 + 3% of the amount
+ * 3 750-5 250          $82.50 + 4%
+ * 5 250-7 000          $142.50 + 5%
+ * Over 7 000           $230 + 6%
  *
- * HINT: Be careful not to display 12:00 as 0:00
  */
 
 #include <stdio.h>
@@ -8,27 +15,25 @@
 
 int main(void)
 {
-    int hour, minutes;
+    float income, tax;
 
-    printf("Enter a 24-hour time (hh:mm): ");
-    scanf("%d:%d", &hour, &minutes);
+    printf("Enter your income : $");
+    scanf("%f", &income);
 
-    if (hour == 0)
-    {// there was tow ways to do this, either changint the variable outside of the printf or inside of it
+    if (income < 750)
+        tax = income * 0.01;
+    else if (income < 2250)
+        tax = 7.50 + income * 0.02;
+    else if (income < 3750)
+        tax = 37.50 + income * 0.03;
+    else if (income < 5250)
+        tax = 82.50 + income * 0.04;
+    else if (income < 7000)
+        tax = 142.50 + income * 0.05;
+    else if (income > 7000)
+        tax = 230 + income * 0.06;
 
-        // hour = 12;
-        printf("Equivalent 12-hour time: %d:%02d AM\n", hour = 12, minutes);
-    }
-    else if (hour >= 1 && hour < 12)
-        { printf("Equivalent 12-hour time: %d:%02d AM\n", hour, minutes); }
-    else if (hour == 12)
-        { printf("Equivalent 12-hour time: %d:%02d PM\n", hour, minutes); }
-    else if (hour > 12)
-    {   // there was tow ways to do this, either changint the variable outside of the printf or inside of it
-
-        // hour -= 12;
-        printf("Equivalent 12-hour time: %d:%02d PM\n", hour -12, minutes);
-    }
+    printf("Your tax amount: %.2f", tax);
 
     return 0;
 }
