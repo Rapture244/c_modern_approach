6c0bc9f85fe6fbf094d57ed18501045e5ef8a5b8
ovidiu.pascal <pascal.ovidiuu@gmail.com>
2024-08-14 20:39:59 +0300

    PROGRAM-C5.1: Calculate & show how many digits a number has
    Signed-off-by: ovidiu.pascal <pascal.ovidiuu@gmail.com>

--- main.c
+++ main.c
@@ -1,54 +1,34 @@
-/* Write a program that reads an integer and displays it in octal (base 8)
+/* Write a program that calculate how many digits a number has
  *
- * The output should be displayed using 5 digits, even if fewer digits are sufficient
+ * You mais assume that the max number is: 4 digits
  *
- * HINT: to convert the number to octal:
- * - Divide the number by 8 and keep track of the quotientt and remainder.
- * - Continue dividing the quotientt by 8 until the quotientt is 0.
- * The octal number will be the sequence of remainders read from bottom to top.
- *
- * (printf is capable of displaying a number in base 8, as we'll see in chapter 7, so there's is actually an easier way to write this program.)
- *
- *
- * Ex: 1953
- * 1953 / 8 = 244 with a remainder of 1
- * 244 / 8 = 30 with a remainder of 4
- * 30 / 8  = 3 with a remainder of 6
- * 3 / 8 = 0 with a remainder of 3
- *
- * Now, read the remainder from bottom to top : 3641
- *
- * So 1953 in octal is 3641
+ * HINT: use if statements to test the number
+ * EX: if the number is between 0 and 9, it has 1 gigit
+ * EX: if the number is between 10 and 99, it has 2 digits
+ * Etc... until 4
  */
 
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(void)
 {
-    int number, digit_1, quotient, digit_2, digit_3, digit_4, digit_5;
+    int number;
 
-    printf("Enter a number between 0 and 32 767: ");
+    printf("Enter a 4 digit number: ");
     scanf("%d", &number);
 
-    digit_1 = number % 8;
-    quotient = number / 8;
+    if (number >= 0 && number <= 9)
+        printf("The number %d has: 1 digit\n", number);
+    else if (number >= 10 && number <= 99)
+        printf("The number %d has: 2 digits\n", number);
+    else if (number >= 100 && number <= 999)
+        printf("The number %d has: 3 digits\n", number);
+    else if (number >= 1000 && number <= 9999)
+        printf("The number %d has: 4 digits\n", number);
+    else
+        printf("The number %d is out of range (more than 4 digits)\n", number);
 
-    digit_2 = quotient % 8;
-    quotient /= 8;
-
-    digit_3 = quotient % 8;
-    quotient /= 8;
-
-    digit_4 = quotient % 8;
-    quotient /= 8;
-
-    digit_5 = quotient % 8;
-
-    // my logical solution but the exercice says to put the 1st remainder last so lets follow it even tho i'm right
-    //printf("In octal, you rnumber is: %05d%d%d%d%d", digit_1, digit_2, digit_3, digit_4, digit_5);
-
-    // Display the number in octal, ensuring 5 digits are shown
-    printf("In octal, you rnumber is: %d%d%d%d%d\n", digit_5, digit_4, digit_3, digit_2, digit_1);
 
     return 0;
 }
